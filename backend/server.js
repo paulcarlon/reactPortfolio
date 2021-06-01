9067549983acaa9de81f7d9ac7b0a4c207d539e7
paulcarlon <paulcarlonlv@gmail.com>
2021-05-31 17:16:22 -0700

    prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -15,11 +15,15 @@ app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/posts', postRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
