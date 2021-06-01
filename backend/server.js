5b652eda6702ef295182ef1e8f9f032cf3f8f682
paulcarlon <paulcarlonlv@gmail.com>
2021-05-31 19:16:01 -0700

    deployment saga

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import path from 'path';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
@@ -15,6 +16,10 @@ app.use(cors());
 
 app.use(express.json());
 
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
+
 app.use('/api/posts', postRoutes);
 
 if (process.env.NODE_ENV === 'production') {
@@ -23,6 +28,10 @@ if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
 }
 
 const PORT = process.env.PORT || 5000;
